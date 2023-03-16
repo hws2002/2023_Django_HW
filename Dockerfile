57ee1d84fe4a7708d5534de0a754152ce25c4ce6
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-03-16 17:27:14 +0800

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,20 @@
 # TODO Start: [Student] Complete Dockerfile
-FROM scratch
+FROM python:3.9
 
-ENV . .
+# 设定 DEPLOY 环境变量为 1
+ENV DEPLOY=1
 
-WORKDIR /
+# 工作目录为 /opt/tmp；
+WORKDIR /opt/tmp
 
-COPY / /
+# 将源代码复制到工作目录
+COPY . .
+# 安装 requirements.txt 声明的依赖并通过换源到 TUNA 加速下载；
+RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
-RUN true
+#对外暴露 80 端口；
+EXPOSE 80
 
-EXPOSE 0
-
-CMD []
+# 容器运行时执行 start.sh 脚本。
+CMD ["sh","start.sh"]
 # TODO End: [Student] Complete Dockerfile
--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@ python3 manage.py makemigrations board
 python3 manage.py migrate
 
 # TODO Start: [Student] Run with uWSGI instead
-python3 manage.py runserver 80
-# uwsgi --module=mysite.wsgi:application \
-#     --env DJANGO_SETTINGS_MODULE=mysite.settings \
-#     --master \
-#     --http=0.0.0.0:0 \
-#     --processes=5 \
-#     --harakiri=20 \
-#     --max-requests=5000 \
-#     --vacuum
+# python3 manage.py runserver 80
+uwsgi --module=DjangoHW.wsgi:application \
+    --env DJANGO_SETTINGS_MODULE=DjangoHW.settings \
+    --master \
+    --http=0.0.0.0:80 \
+    --processes=5 \
+    --harakiri=20 \
+    --max-requests=5000 \
+    --vacuum
 # TODO End: [Student] Run with uWSGI instead
