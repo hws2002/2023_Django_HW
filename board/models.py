e3b1695b41071fa0bbe960b5d7717304dc8ca6e5
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-03-16 17:20:56 +0800

    models.py

--- board/models.py
+++ board/models.py
@@ -30,17 +30,23 @@ class User(models.Model):
 
 class Board(models.Model):
     # TODO Start: [Student] Finish the model of Board
-    
     # id, BigAutoField, primary_key=True
     # user, ForeignKey to User, CASCADE deletion
     # board_state, CharField
     # board_name, CharField
     # created_time, FloatField, default=utils_time.get_timestamp
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board_state = models.CharField(max_length=MAX_CHAR_LENGTH)
+    board_name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    created_time = models.FloatField(default=utils_time.get_timestamp)
     
     # Meta data
     # Create index on board_name
     # Create unique_together on user and board_name
-    
+    class Meta:
+        indexes = [models.Index(fields=["board_name"])]
+        unique_together = ["user", "board_name"]
     # TODO End: [Student] Finish the model of Board
 
     def serialize(self):
