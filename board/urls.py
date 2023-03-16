3ec825da5652244f88180b922168b42e7f12b896
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-03-16 17:20:27 +0800

    urls.py

--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('startup', views.startup),
     path('boards', views.boards),
     # TODO Start: [Student] add routing paths for `boards/<index>` and `user/<userName>`
-
+    path('boards/<index>',views.boards_index),
+    path('user/<name>',views.user_board), #after filling up board/views.py
     # TODO End: [Student] add routing paths for `boards/<index>` and `user/<userName>`
 ]
